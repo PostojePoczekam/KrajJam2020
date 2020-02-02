9ace416f73a7aacc117e8d7a1e67618cca7efb8e
siewcasalwus <siewcasalwus@gmail.com>
2020-02-02 15:13:35 +0100

    *: final v1

---
+++ Assets/TableParticleSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableParticleSpawner : MonoBehaviour {
+
+    public ParticleSystem dropParticles;
+    
+    private void OnCollisionEnter(Collision other) {
+        if (other.collider.GetComponent<Interactable>() == null) return;
+
+        var newPart = Instantiate(dropParticles);
+        newPart.gameObject.SetActive(true);
+        newPart.transform.position = other.contacts[0].point;
+        newPart.Play();
+        Destroy(newPart, 2f);
+    }
+}
