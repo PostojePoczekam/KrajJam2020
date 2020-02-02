4eef30b998472c17c42d4bbc0c9844d8fba8fe95
Postoje Poczekam <mactwo93@gmail.com>
2020-02-02 02:45:02 +0100

    +bound rect

--- Assets/Scripts/ArmController.cs
+++ Assets/Scripts/ArmController.cs
@@ -11,12 +11,21 @@ public class ArmController : MonoBehaviour
 	[SerializeField] private Rigidbody _hand;
 	[SerializeField] private float _movementSpeed = 0.1f;
 	[SerializeField] private int _playerIndex;
+
+	private BoundingRect _bounds;
 	private Gamepad _gamepad;
 
 
 	private Vector2 _movementVector;
 
-	private void MoveArm(Vector2 delta) => _hand.MovePosition(_hand.position + new Vector3(delta.x, 0f, delta.y) * _movementSpeed);
+	private void MoveArm(Vector2 delta)
+	{
+		var newPos = _hand.position + new Vector3(delta.x, 0f, delta.y) * _movementSpeed;
+		Debug.Log(newPos);
+		if (_bounds.CanMove(newPos))
+			_hand.MovePosition(newPos);
+	}
+
 	private void RotateArm(Vector2 forward)
 	{
 		if (forward.magnitude > 0.1f)
@@ -27,24 +36,27 @@ public class ArmController : MonoBehaviour
 	{
 		_interactionHand.Grab();
 	}
-	
-	private void ReleaseGrab() {
+
+	private void ReleaseGrab()
+	{
 		_interactionHand.Release();
 	}
 
 	private void Update()
 	{
 		if (Gamepad.all.ToArray().Length <= _playerIndex) return;
-		
+
 		MoveArm(_gamepad.leftStick.ReadValue());
 		RotateArm(_gamepad.rightStick.ReadValue());
 		if (_gamepad.rightTrigger.wasPressedThisFrame) Grab();
 		if (_gamepad.rightTrigger.wasReleasedThisFrame) ReleaseGrab();
 	}
 
-	private void Awake() {
+	private void Awake()
+	{
+		_bounds = FindObjectOfType<BoundingRect>();
 		if (Gamepad.all.ToArray().Length <= _playerIndex) return;
-		
+
 		_gamepad = Gamepad.all.ToArray()[_playerIndex];
 	}
 }
